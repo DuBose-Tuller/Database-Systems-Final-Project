4530d88d514af1986e3bd747ddb24015d71a7f17
DuBose Tuller <d.tuller@wustl.edu>
2023-12-06 10:30:19 -0500

    qb schema runs!

--- QuizbowlSchema.sql
+++ QuizbowlSchema.sql
@@ -3,71 +3,65 @@ CREATE SCHEMA qb;
 USE qb;
 
 CREATE TABLE player (
-	player_id		INT NOT NULL AUTO_INCREMENT,
-	name		VARCHAR(100) NOT NULL,
-	major		VARCHAR(25) CHECK (major IN ('Science', 'History', 'Literature'))
-	minor		VARCHAR(25) CHECK (minor IN ('RMP', 'Geography', 'FA', 'Trash'))
+	id		INT NOT NULL AUTO_INCREMENT,
+	name				VARCHAR(100) NOT NULL,
+	major				VARCHAR(25) CHECK (major IN ('Science', 'History', 'Literature')),
+	minor				VARCHAR(25) CHECK (minor IN ('RMP', 'Geography', 'FA', 'Trash')),
 	 	
-	PRIMARY KEY (player_id)	
+	PRIMARY KEY (id)	
 );
 
 CREATE TABLE team (
-	team_id,
-	name
+	id			INT NOT NULL AUTO_INCREMENT,
+	name				VARCHAR(100) NOT NULL,
+	school 			VARCHAR(50),
+
+	PRIMARY KEY (id)
 );
 
+-- Joint relation between player and team
 CREATE TABLE playsOn (
-	player_id,
-	team_id,
+	player_id	INT,
+	team_id		INT,
 	
-FOREIGN KEY (team_id) 
-		REFERENCES team(team_id)
-		ON DELETE SET NULL,
+	FOREIGN KEY (team_id) 
+			REFERENCES team(id),
 
 	FOREIGN KEY (player_id) 
-		REFERENCES player(player_id)
-		ON DELETE SET NULL,
-)
+		REFERENCES player(id),
+
+	PRIMARY KEY(player_id, team_id)
+);
 
 CREATE TABLE qb_match(
-	match_id		INT NOT NULL AUTO_INCREMENT,
+	id		INT NOT NULL AUTO_INCREMENT,
 	datetime		DATETIME,
 	winner_id		INT,
 	loser_id		INT,
 		
 	FOREIGN KEY (winner_id) 
-		REFERENCES team(team)
-		ON DELETE SET NULL,
+		REFERENCES team(id),
 		
 	FOREIGN KEY (loser_id) 
-		REFERENCES team(id)
-		ON DELETE SET NULL,
+		REFERENCES team(id),
 		
-	PRIMARY KEY (match_id)
+	PRIMARY KEY (id)
 );
 
-
+-- Joint relation between player and match
 CREATE TABLE plays (
 	player_id			INT NOT NULL,
 	match_id			INT NOT NULL,
-	
-	is_winner			VARCHAR(1) NOT NULL,
-	aces				INT,
-	double_faults		INT,
-	first_in			INT,
-	first_won			INT,
-	second_won			INT,
-	service_games		INT,
-	breakpoints_saved	INT,
-	breakpoints_faced	INT,
-	player_rank			INT CHECK(player_rank > 0), 	
-	rank_points			INT CHECK(rank_points >= 0),
+	num_powers		INT,
+	num_tens		INT,
+	num_negs		INT,
+	num_tossups_heard	INT,
 	
 	FOREIGN KEY (player_id) 
-		REFERENCES player(player_id),
+		REFERENCES player(id),
 		
 	FOREIGN KEY (match_id)
-		REFERENCES qb_match(match_id),
+		REFERENCES qb_match(id),
 		
 	PRIMARY KEY (player_id, match_id)
 );
