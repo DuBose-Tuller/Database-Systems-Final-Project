66f2a34ffeea5ddf0aa11d035fb365a62b79e211
DuBose Tuller <d.tuller@wustl.edu>
2023-12-14 15:55:45 -0500

    populate the match page

--- queries/getPlayerAndTeamNamesFromMatchID.sql
+++ queries/getPlayerAndTeamNamesFromMatchID.sql
@@ -1,11 +1,12 @@
 SELECT 
 	qb_match.match_id, 
 	playson.username, 
-	team.name,
-FROM qb_match 	INNER JOIN team
-					ON team.team_id = qb_match.home_id OR team.team_id = qb_match.away_id 
-				INNER JOIN playson
-					ON team.team_id = playson.team_id 			
-				
-WHERE qb_match.match_id = <match_id>
+	team.name 
+FROM 
+	qb_match 
+	INNER JOIN team
+		ON team.team_id = qb_match.home_id OR team.team_id = qb_match.away_id 
+	INNER JOIN playson
+		ON team.team_id = playson.team_id 			
+WHERE qb_match.match_id = <match_id> 
 ORDER BY team.name DESC;
